7e55ae0a5c8951f1f4e704b83d5ff5ac4d7b6b28
m-naveed-ashraf <mahernaveed531@gmail.com>
2024-10-31 09:46:38 +0500

    bulk_fetch prompt templates

--- create_plot.py
+++ create_plot.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-def create_plot(timestamps: list):
+def create_plot(timestamps: list, title):
     # Separate timestamps into start and end times
     events = {}
     for event, elapsed_time, type in timestamps:
@@ -14,12 +14,18 @@ def create_plot(timestamps: list):
     for i, (event, times) in enumerate(events.items()):
         start = times['start']
         end = times['end']
+        
+        # Skip plotting if either start or end time is missing
+        if start is None or end is None:
+            print(f"Warning: Missing start or end time for event '{event}'")
+            continue
+        
         duration = end - start
         ax.barh(event, duration, left=start, align='center')
 
     # Setting labels for milliseconds display
     ax.set_xlabel("Time (ms)")
     ax.set_ylabel("Event")
-    plt.title("Parallel Execution of Async Tasks (Time in ms)")
+    plt.title(title or "Parallel Execution of Async Tasks (Time in ms)")
     plt.tight_layout()
     plt.show()
